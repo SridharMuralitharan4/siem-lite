9c48e20844839ed5aff77e8c7d6703ef64694e09
SridharMuralitharan4 <sridharmuralitharan67@gmail.com>
2026-04-22 16:02:08 +0000

    Added HTTP log ingestion

--- sender.py
+++ sender.py
@@ -1,33 +1,39 @@
 import requests
-import time
 
 print("=== SIEM HTTP Agent ===")
 
-URL = input("Enter SIEM Server URL (example: https://your-app.onrender.com): ").strip()
-
+URL = input("Enter SIEM Server URL: ").strip()
 endpoint = f"{URL}/log"
 
-attacks = [
-    """Image: C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe
-CommandLine: powershell -EncodedCommand ZQBjAGgAbwAgIkhBQ0tFRCI=""",
-
-    """Image: C:\\Windows\\System32\\cmd.exe
-CommandLine: cmd.exe /c powershell -EncodedCommand ZQBjAGgAbwAgIkhBQ0tFRCI=""",
-
-    """Image: C:\\Windows\\explorer.exe
-CommandLine: explorer.exe"""
-]
-
-i = 0
-
 while True:
-    log = attacks[i % len(attacks)]
+    print("\n1. Normal Activity")
+    print("2. Medium Activity")
+    print("3. HIGH Attack (Encoded PowerShell)")
+    print("4. Exit")
+
+    choice = input("Select: ").strip()
+
+    if choice == "1":
+        log = """Image: C:\\Windows\\explorer.exe
+CommandLine: explorer.exe"""
+
+    elif choice == "2":
+        log = """Image: C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe
+CommandLine: powershell Get-Process"""
+
+    elif choice == "3":
+        log = """Image: C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe
+CommandLine: powershell -EncodedCommand ZQBjAGgAbwAgIkhBQ0tFRCI="""
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Invalid")
+        continue
 
     try:
         r = requests.post(endpoint, json={"log": log})
         print("[+] Sent:", r.json())
     except Exception as e:
         print("[!] Failed:", e)
-
-    i += 1
-    time.sleep(5)
